ebdde793433a490d368f2f501f04681dccdc8d38
Carlos Colombo <csc7@hw.ac.uk>
2022-02-25 17:37:04 +0000

    Initial commit

---
+++ PP5_CI_GDEC/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PP5_CI_GDEC project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PP5_CI_GDEC.settings')
+
+application = get_wsgi_application()

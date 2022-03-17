0cfb57604f6d4dc6f709e85af64c4abd9c561e29
Carlos Colombo <csc7@hw.ac.uk>
2022-03-17 18:45:41 +0000

    Add success toast for updating and removing items

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -27,20 +27,25 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if resolution in bag[item_id]['items_by_resolution'].keys():
                 bag[item_id]['items_by_resolution'][resolution] += quantity
+                messages.success(request, f'ADD_TO_BAG Updated {resolution.upper()} resolution quantity for {bag[item_id]["items_by_resolution"]}')
             else:
                 bag[item_id]['items_by_resolution'][resolution] = quantity
+                messages.success(request, f'ADD_TO_BAG {resolution.upper()} resolution for {product.name} added to the bag')
         else:
             bag[item_id] = {'items_by_resolution': {resolution: quantity}}
+            messages.success(request, f'ADD_TO_BAG {resolution} resolution for {product.name} added to the bag')
             
 
     else:
 
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'ADD_TO_BAG Updated {product.name} quantity to {bag[item_id]}')
+
 
         else:
             bag[item_id] = quantity
-            messages.success(request, f'{product.name} added to the bag')
+            messages.success(request, f'ADD_TO_BAG {product.name} added to the bag')
             
 
     request.session['bag'] = bag
@@ -51,24 +56,33 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of items in the purchasing bag"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     resolution = None
     if 'product_resolution' in request.POST:
         resolution = request.POST['product_resolution']
+        
     bag = request.session.get('bag', {})
 
     if resolution:
         if quantity > 0:
             bag[item_id]['items_by_resolution'][resolution] = quantity
+            messages.success(request, f'Updated {resolution.upper()} resolution quantity for {bag[item_id]["items_by_resolution"]}')
         else:
             del bag[item_id]['items_by_resolution'][resolution]
             if not bag[item_id]['items_by_resolution']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed {resolution.upper()} resolution for {product.name} from the bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            
         else:
             bag.pop(item_id)
+            messages.success(request, f'{product.name} removed from the bag')
+            print("RESOLUTON =")
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -78,6 +92,7 @@ def remove_from_bag(request, item_id):
     """Remove items from the purchasing bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         resolution = None
         if 'product_resolution' in request.POST:
             resolution = request.POST['product_resolution']
@@ -87,11 +102,15 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_resolution'][resolution]
             if not bag[item_id]['items_by_resolution']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed {resolution.upper()} resolution for {product.name} from the bag')
         else:
             bag.pop(item_id)
+            print("REMOVING")
+            messages.success(request, f'{product.name} removed from the bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item_ {e}')
         return HttpResponse(status=500)

ad142ba7b86fc913ec709cb1bcf600b3a215d838
Carlos Colombo <csc7@hw.ac.uk>
2022-03-23 04:14:14 +0000

    Create and start working in user profiles

--- profiles/views.py
+++ profiles/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import render, get_object_or_404
 
 # INTERNAL:
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 ###############################################################################
@@ -25,9 +26,14 @@ def profile(request):
     """ Display User Profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)

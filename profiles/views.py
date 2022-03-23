fe17e2a854b4cd50eda6b800f0635592f0410c53
Carlos Colombo <csc7@hw.ac.uk>
2022-03-23 03:46:28 +0000

    Change blockcontent of allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -9,9 +9,10 @@ Django views for the checkout app
 # IMPORTED RESOURCES #
 
 # EXTERNAL:
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # INTERNAL:
+from .models import UserProfile
 
 
 ###############################################################################
@@ -22,8 +23,11 @@ from django.shortcuts import render
 
 def profile(request):
     """ Display User Profile """
-    
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)

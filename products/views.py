45ea436fc77382b6ad959321e032bdb9d7990a8b
Carlos Colombo <csc7@hw.ac.uk>
2022-03-24 16:02:14 +0000

    Add management/admin option to edit products

--- products/views.py
+++ products/views.py
@@ -111,3 +111,29 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit products of the store """
+    # Prefill the form
+    product = get_object_or_404(Product, pk=product_id)
+    # Instantiate the form if method is POST
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product successfully updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to edit product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)

66af59f70bd39a101585824f46a402fbf2c707e7
Carlos Colombo <csc7@hw.ac.uk>
2022-04-02 21:35:25 +0000

    Change label for cancelling delivery based on checkbox status

--- static/js/download_data_and_discount_delivery.js
+++ static/js/download_data_and_discount_delivery.js
@@ -1,16 +1,12 @@
 console.log("Download data loaded")
 
-
-
 $('#digital').change(function() {
     let status = $("#digital").is(':checked');
     console.log(status);
     this.form.submit();
     //if (status) {
-    //    $('#digital').prop('checked', true);
+    //    $('#cancel_delivery_cost>label').html("Check this box to download the data and avoid shipment costs");
     //} else {
-    //    $('#digital').prop('checked', false);
-    //}
-    
-    
+    //    $('#cancel_delivery_cost>label').html("Uncheck this box to receive the data in USB drives (delivery costs apply)");
+    //}    
 });

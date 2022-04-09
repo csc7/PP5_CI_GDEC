d73ebf8d59cfde58a39b607b707e1019131d954d
Carlos Colombo <csc7@hw.ac.uk>
2022-04-09 21:18:53 +0000

    Add style to titles, input fields and buttons of allauth Django forms and pages

--- PP5_CI_GDEC/settings.py
+++ PP5_CI_GDEC/settings.py
@@ -238,9 +238,11 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+# For development:
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'gdec@gdectestexample.com'
+
 # For production:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

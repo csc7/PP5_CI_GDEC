a42cacf89d437c65e23007f19666ad4763efe331
Carlos Colombo <csc7@hw.ac.uk>
2022-04-10 01:33:32 +0000

    Correct style in top banner to avoid double rows in the single row banner

---
+++ static/js/messages-in-top-banner.js
@@ -0,0 +1,18 @@
+//var i = 1;
+//var animationTime = 20000; // Ensure it matches the length value (time) in the animation given by the base.css file (.last-news)
+//
+//
+//        setTimeout(function() { 
+//	    $("#banner-message").html("Our deliveries include insurance");
+//        }, 10000);
+
+
+
+
+
+    
+
+
+
+
+

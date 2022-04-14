0427053ec30ccd2857b8dc70f1c28edcb9fb5dff
Carlos Colombo <csc7@hw.ac.uk>
2022-04-14 22:59:31 +0000

    Change colour to wish list icon in navigation menu and convert product names into links

--- wish/contexts.py
+++ wish/contexts.py
@@ -23,30 +23,11 @@ from products.models import Product
 
 def wish_list_contents(request):
     """
-    This function computes the grand total amount based on the products
-    in the bag and the user's delivery preferences
+    This function computes the quantities and price per item
+    in the wish list
     """
 
-    # Check if user has activated the option to receive the data
-    # digitally and therefore cancel delivery costs
-    #if request.method == 'POST':
-    #    cancel_delivery_cost = request.POST.get('digital', False)
-    #    if cancel_delivery_cost:
-    #        cancel_delivery_cost_factor = 0
-    #    else:
-    #        cancel_delivery_cost_factor = 1
-    #    print(cancel_delivery_cost)
-#
-    #else:
-    #    cancel_delivery_cost = request.POST.get('digital', False)
-    #    if cancel_delivery_cost:
-    #        cancel_delivery_cost_factor = 0
-    #    else:
-    #        cancel_delivery_cost_factor = 1
-    #    print(cancel_delivery_cost)
-
-
-    # Initialize bag item and costs amounts
+    # Initialize wish list items and costs amounts
     wish_list_items = []
     total = 0
     product_count = 0
@@ -77,37 +58,13 @@ def wish_list_contents(request):
                     'product': product,
                     'resolution': resolution,
                 })
-
-    #if 'product_resolution' in request.POST:
-    #    print("OK")
-
-    # Compute grand total
-    #if total < settings.DISCOUNT_THRESHOLD:
-    #    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
-    #    delivery = delivery * cancel_delivery_cost_factor
-    #    discount = 0
-    #    delta_for_discount = settings.DISCOUNT_THRESHOLD - total
-    #else:
-    #    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
-    #    delivery = delivery * cancel_delivery_cost_factor
-    #    discount = total * Decimal(settings.DISCOUNT_PERCENTAGE/100)
-    #    delta_for_discount = 0
-    #
-    #grand_total = total + delivery - discount
-    
+  
 
     # Return context to bag template
     context = {
         'wish_list_items': wish_list_items,
         'total': total,
         'product_count': product_count,
-        #'delivery': delivery,
-        #'delta_for_discount': delta_for_discount,
-        #'discount_threshold': settings.DISCOUNT_THRESHOLD,
-        #'grand_total': grand_total,
-        #'discount': discount,
-        #'discount_percentage': settings.DISCOUNT_PERCENTAGE,
-        #'cancel_delivery_cost': cancel_delivery_cost,
     }
 
     return context

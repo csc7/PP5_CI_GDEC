771925b0e31a9bf8ebf8936a7af86d9627353b0d
Carlos Colombo <csc7@hw.ac.uk>
2022-04-25 23:36:10 +0000

    Correct validation images of contact app

--- contact/apps.py
+++ contact/apps.py
@@ -9,11 +9,8 @@ from django.apps import AppConfig
 
 
 class ContactConfig(AppConfig):
-    """ 
+    """
     Configuration class for the contact app
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
-
-
-
--- contact/forms.py
+++ contact/forms.py
@@ -22,8 +22,10 @@ class ContactForm(forms.ModelForm):
     """
     Class for a Simple Contact Form with Django
     """
-    
-    # Definition based on this article: https://studygyaan.com/django/how-to-use-bootstrap-4-forms-with-django-crispy-forms#html-code,
+
+    # Definition based on this article:
+    # https://studygyaan.com/django/
+    # how-to-use-bootstrap-4-forms-with-django-crispy-forms#html-code,
     # StudyGyaan, accessed on April 16th, 2022.
     class Meta:
         model = DataFromContactForm

008446d4f09808812e8a1fa26e7b0da6ab5927fe
Carlos Colombo <csc7@hw.ac.uk>
2022-04-29 14:38:58 +0000

    Set DEBUG to 0

--- PP5_CI_GDEC/settings.py
+++ PP5_CI_GDEC/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = 1
+DEBUG = 0
 
 ALLOWED_HOSTS = [
     'pp5-ci-gdec.herokuapp.com',
